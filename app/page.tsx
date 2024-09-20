74eaca9c612a790a5b24ee6bef082a54df4437c2
grzla <greg.j.lundberg@gmail.com>
2024-09-20 10:51:40 -0400

    update logo and theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Hopper</h1>
+      <Image src="/images/heytherr.svg" alt="Hopper" width={400} height={400} className="mb-8" />
       <div className="flex gap-4">
-        <Link href="/map" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        <Link href="/map" className="px-4 py-2 bg-hey-red text-white rounded hover:bg-blue-600">
           View Map
         </Link>
-        <Link href="/business" className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+        <Link href="/business" className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-600">
           Business Portal
         </Link>
       </div>
